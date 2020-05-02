55cb4a160addf6e09d452ad5bd35c8f93b2c8fbd
EnderPearlMC <48450374+EnderPearlMC@users.noreply.github.com>
2020-05-02 21:38:35 +0200

    Add files via upload

---
+++ CodeEasier/Scene/ICESceneDrawable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+   _____          _        ______          _           
+  / ____|        | |      |  ____|        (_)          
+ | |     ___   __| | ___  | |__   __ _ ___ _  ___ _ __ 
+ | |    / _ \ / _` |/ _ \ |  __| / _` / __| |/ _ \ '__|
+ | |___| (_) | (_| |  __/ | |___| (_| \__ \ |  __/ |   
+  \_____\___/ \__,_|\___| |______\__,_|___/_|\___|_|   
+                                                      
+
+    Made by EnderPearl MC
+
+     This framework allows you to create games very quickly.
+     Made with monogame.
+
+     You are free to use this framework in all your projects
+     but you cannot REDISTRIBUTE it. 
+
+ */
+
+namespace CodeEasier.Scene
+{
+
+    /*
+
+        SceneDrawable interface
+
+        Usage : All the components that will be drawn in a scene must implement this interface
+
+    */
+
+    interface ICESceneDrawable
+    {
+
+        void Load();
+        void Update(GameTime gameTime);
+        void Draw();
+
+    }
+}
