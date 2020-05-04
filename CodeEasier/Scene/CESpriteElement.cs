8b73d630a7de8143389779f7e921736e3d37b48a
EnderPearlMC <48450374+EnderPearlMC@users.noreply.github.com>
2020-05-04 11:22:09 +0200

    Add files via upload

---
+++ CodeEasier/Scene/CESpriteElement.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+/*
+   _____          _        ______          _           
+  / ____|        | |      |  ____|        (_)          
+ | |     ___   __| | ___  | |__   __ _ ___ _  ___ _ __ 
+ | |    / _ \ / _` |/ _ \ |  __| / _` / __| |/ _ \ '__|
+ | |___| (_) | (_| |  __/ | |___| (_| \__ \ |  __/ |   
+  \_____\___/ \__,_|\___| |______\__,_|___/_|\___|_|   
+                                                      
+
+    Made by EnderPearl MC
+
+     This framework allows you to create games very quickly.
+     Made with monogame.
+
+     You are free to use this framework in all your projects
+     but you cannot REDISTRIBUTE it. 
+
+ */
+
+namespace CodeEasier.Scene
+{
+    /*
+
+    ImageElement class
+
+    Create an image element (can be used for backgrounds)
+
+    Usage : Instance it.
+
+    */
+
+    abstract class CESpriteElement : ICESceneDrawable
+    {
+
+        /*
+         *  Properties 
+         */
+
+        public Texture2D Texture { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Vector2 Position { get; set; }
+        public float VX { get; set; }
+        public float VY { get; set; }
+
+        /*
+         *  Constructor
+         */
+
+        /**
+         * <param type="Texture2D" name="texture">The texture of the image</param>
+         * <param type="Vector2" name="position">The position of the image</param>
+         * <param type="int" name="width">The width of the sprite</param>
+         * <param type="int" name="height">The height of the sprite</param>
+         */
+        public CESpriteElement(Texture2D texture, Vector2 position, int width, int height)
+        {
+            Texture = texture;
+            Position = position;
+            Width = width;
+            Height = height;
+        }
+
+        /*
+         *  Methods 
+         */
+
+        public virtual void Load()
+        {
+            
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Position = new Vector2(Position.X + VX, Position.Y + VY);
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new Rectangle((int)Math.Round(Position.X), (int)Math.Round(Position.Y), Width, Height), Color.White);
+        }
+
+    }
+}
